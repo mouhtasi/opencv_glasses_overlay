8773d9bf2214459e2e1e24c43bb804583edfb4d8
Imad Mouhtassem <mouhtasi@gmail.com>
2013-01-10 23:49:57 -0500

    Draws rectangle outlining eye-pair area instead of simply printing that it was detected.

--- facedetect.py
+++ facedetect.py
@@ -1,25 +1,42 @@
 import pyopencv
 
+def draw(frame, eyes):
+    r = eyes[0]
+    pt1 = pyopencv.Point()
+    pt2 = pyopencv.Point()
+    pt1.x = r.x
+    pt2.x = r.x + r.width
+    pt1.y = r.y
+    pt2.y = r.y + r.height
+
+    center = pyopencv.Point(int(round(r.x + r.width * 0.5)),
+                            int(round(r.y + r.height * 0.5)))
+    #radius = int(round((r.width + r.height) * 0.25))
+    pyopencv.rectangle(frame, pt1, pt2, pyopencv.CV_RGB(255, 0, 0), 2,
+                    pyopencv.CV_AA, 0)
+
+    pyopencv.imshow(winName, frame)
+
 def detect(frame, cascade, fallbackCascade):
     minPairSize = pyopencv.Size(20, 20)
 
     haarScale = 1.1
-    imageScale = 2
+    minNeighbors = 0
     haarFlags = pyopencv.CascadeClassifier.FIND_BIGGEST_OBJECT
 
     grayImg = pyopencv.Mat()
     pyopencv.cvtColor(frame, grayImg, pyopencv.CV_BGR2GRAY)
     pyopencv.equalizeHist(grayImg, grayImg)
 
-    eyes = cascade.detectMultiScale(grayImg, haarScale, imageScale, haarFlags,
+    eyes = cascade.detectMultiScale(grayImg, haarScale, minNeighbors, haarFlags,
                                     minPairSize)
 
     if len(eyes) == 0:
-        eyes = fallbackCascade.detectMultiScale(grayImg, haarScale, imageScale,
-                                                haarFlags, minPairSize)
+        eyes = fallbackCascade.detectMultiScale(grayImg, haarScale,
+                                        minNeighbors, haarFlags|pyopencv.CascadeClassifier.SCALE_IMAGE, minPairSize)
 
     if len(eyes) != 0:
-        print 'Eye-pair detected!'
+        draw(frame, eyes)
 
 def capture(winName, cascade, fallbackCascade):
     capture = pyopencv.VideoCapture()
